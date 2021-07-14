40df73ca2d42124be826b526c47c92c1b8cbcc95
Abrão Astério Junior <abcorinthians@gmail.com>
2021-07-14 02:30:51 -0300

    Developed unit tests for create Person

--- src/main/java/dev/byAbrao/personapi/services/PersonService.java
+++ src/main/java/dev/byAbrao/personapi/services/PersonService.java
@@ -6,6 +6,7 @@ import dev.byAbrao.personapi.entities.Person;
 import dev.byAbrao.personapi.exceptions.PersonNotFoundException;
 import dev.byAbrao.personapi.mapper.PersonMapper;
 import dev.byAbrao.personapi.repositories.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +14,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
---
+++ src/test/java/dev/byAbrao/personapi/services/PersonServiceTest.java
@@ -0,0 +1,47 @@
+package dev.byAbrao.personapi.services;
+
+import dev.byAbrao.personapi.dto.request.PersonDTO;
+import dev.byAbrao.personapi.dto.response.MessageResponseDTO;
+import dev.byAbrao.personapi.entities.Person;
+import dev.byAbrao.personapi.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static dev.byAbrao.personapi.utils.PersonUtils.createFakeDTO;
+import static dev.byAbrao.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Created person with ID " + savedPersonId)
+                .build();
+    }
+}
